dd600a768a7c7f7417b78e767df8fde09817d21c
Robert D Gauthier <rgauthie@gmail.com>
2018-03-26 05:03:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,7 +132,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    puts "Congrats #{winner(board)}"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Draw"
   end
